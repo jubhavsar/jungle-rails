fc36980922438e48533073c3de17b237aefefedb
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-09 14:59:28 -0500

    Fixed user authentication bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,21 @@ include BCrypt
 
    has_secure_password
 
-   validates :password, presence: true
-   validates :password_confirmation, presence: true
-   validates :password, confirmation: true
+  #  validates :password, presence: true
+  #  validates :password_confirmation, presence: true
+  #  validates :password, confirmation: true
    validates :password, length: { minimum: 6 }  
-   validates :email, uniqueness: true
-   validates :email, presence: true
+  #  validates :email, uniqueness: true
+   validates :email, presence: true, uniqueness: true
    validates :name, presence: true
 
 
-   def authenticate_with_credentials(userEmail, userPassword)
+   def self.authenticate_with_credentials(userEmail, userPassword)
      # check if exists in the db return user object or nil
 
      user = User.find_by(email: userEmail)
 
-     if (Password.new(user[:password_digest]).to_s == userPassword)
+     if (user and user.authenticate(userPassword))
        return user
      else
        return nil
